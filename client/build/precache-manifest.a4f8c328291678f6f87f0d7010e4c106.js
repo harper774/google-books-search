cddbf289639e36306b66b6e02d91e837cf794a83
Harper <harperliulove@gmail.com>
2020-03-27 16:46:09 +1100

    build

---
+++ client/build/precache-manifest.a4f8c328291678f6f87f0d7010e4c106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b5d59980915723313d5",
+    "url": "/static/css/main.c2d667af.chunk.css"
+  },
+  {
+    "revision": "8b5d59980915723313d5",
+    "url": "/static/js/main.3eefdcd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0d5c17320b3994529e0",
+    "url": "/static/js/2.998b6ee3.chunk.js"
+  },
+  {
+    "revision": "5609afb59e9e2ece8fa8c32a44402004",
+    "url": "/index.html"
+  }
+];
